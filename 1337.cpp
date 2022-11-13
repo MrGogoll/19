e1b44e226d86ed7572ac8c197cf78e6b2f716c58
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-13 15:59:31 +0300

    19

---
+++ 1337.cpp
@@ -0,0 +1,19 @@
+﻿// 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b,c;
+    cin >> a >> b;
+	c = 0;
+	for (int i = a-1; i < b; i++)
+	{
+		if (i % 100 == 00|| i % 100 == 11|| i % 100 == 22|| i % 100 == 33|| i % 100 == 44|| i % 100 == 55|| i % 100 == 66|| i % 100 == 77|| i % 100 == 88|| i % 100 == 99) {
+			c += 1;
+		}
+	}
+	cout << c;
+
+}
+
